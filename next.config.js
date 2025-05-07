c7e9e303908a8b6bcadf1377e8333229cf02064d
joffewilliam <78730245+joffewilliam@users.noreply.github.com>
2025-05-07 08:39:24 -0400

    Maybe this will work...

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
   // Use the repository name for base path in production
   basePath: process.env.NODE_ENV === 'production' ? '/WPMGAME' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/WPMGAME/' : '',
+  trailingSlash: false, // Recommended for static exports to avoid issues with path resolution
 };
 
 module.exports = nextConfig;
